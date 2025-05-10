57d58f3b5144e26425b16636091b29150c8e450e
Jacob <jacobzhen7@gmail.com>
2025-05-09 23:20:53 -0600

    I dont even know anymore

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from app import services
 
 router = APIRouter()
 
 @router.get("/")
 def read_root():
     return {"message": "Welcome to Fantasy Wrapped!"}
+
+@router.get("/find-trae")
+def find_trae_young_route(team):
+    return services.find_trae_young(team) # im like 90% sure this is wrong
+
+@router.get("/team-points")
+def get_team_points_route(team):
+    return services.get_total_points(team) # im like 90% sure this is wrong
--- app/services.py
+++ app/services.py
@@ -1,16 +1,37 @@
 from espn_api.basketball import League
 import requests
 
-def find_trae_young(league_id: int, season_id: int, espn_s2: str, swid: str):
+def get_league(league_id: int, season_id: int, espn_s2: str, swid: str) -> League:
+    if not hasattr(get_league, "league"):
+        get_league.league = League(
+            league_id=league_id,
+            season_id=season_id,
+            espn_s2=espn_s2,
+            swid=swid
+        )
+    return get_league.league
+    # what the freak is dependency injection
 
-    "Im gonna find trae young!"
 
-    league = League(league_id=league_id, season_id=season_id, espn_s2=espn_s2, swid=swid)
-    players = league.players
-    for player in players:
-        if "Trae Young" in player.name.lower():
+def get_team_by_name(league: League, team_name: str):
+    for team in league.teams:
+        if team_name.lower() in team.team_name.lower():
+            return team
+    return None
+
+def find_trae_young(team):
+
+    #Im gonna find trae young!
+    for player in team.roster:
+        if "trae young" in player.name.lower():
             return {
                 "found trae young!"
             }
         
-    return {"didn't find trae young"}
+    return {"didn't find trae young aw man"}
+
+def get_total_points(team):
+    total_points = sum(player.fantasy_points for player in team.roster)
+    return {
+        "total_fantasy_points": total_points
+    }
