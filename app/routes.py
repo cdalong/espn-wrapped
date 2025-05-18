e47c849cd1a56ecee879ca00acc25121c56f8eb8
Jacob <jacobzhen7@gmail.com>
2025-05-17 21:06:48 -0600

    Methods work now, adjusted routes accordingly

--- app/routes.py
+++ app/routes.py
@@ -1,94 +1,155 @@
 from fastapi import APIRouter, Depends, HTTPException
-from app import services
+from pydantic import BaseModel
+from app.services import Services
 from espn_api.basketball import League
 
 router = APIRouter()
+services = Services(league_id=332773775, year=2025, espn_s2='AEB%2FraVAzJUuPQQx%2FZbZyHlQBgCLq%2FRJZeRW%2FD2PS9L1c89tj7UCmG7Y8jGvoYKhToVRtrWmOV8wHyGr8PkOlQJ%2Bc6WyPrTJHE8s2fgroHPV2Z3vA3Hp1QbO0ZlHFu0YvNBT1OMvExX1l7vPZPi5Is4Fmqx8AJDu8aGb5sdXtY5G1oEJ5imB9sjcwj3QUnA0lBdCWbQ%2BUcs%2FDnBNkWDd%2Fe191amCJFp7S0%2BnH1ut5HMOPlo%2B6gh3FhScoJQNIhqkGL2gQr0Bv0WIrSA%2F7Cg8ywpJPBwCDr9tpfwAmfqFWYzABQ%3D%3D', swid='{1A576FEF-EB0A-4EAC-A122-54A7CB7DD0FF}')
 
 @router.get("/")
 def read_root():
     return {"message": "Welcome to Fantasy Wrapped!"}
 
+class ServiceParams(BaseModel):
+    league_id: int
+    year: int
+    espn_s2: str
+    swid: str
 
-#lowkey chagpted like all of this
-@router.get("/team/{team_name}/find-trae")
-def find_trae_young_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    return services.find_trae_young(team)
+@router.get("/team/find-trae")
+def find_trae_young_route(params: ServiceParams):  
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.find_trae_young()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/team/{team_name}/points")
-def get_team_points_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    return services.get_total_points(team)
+@router.get("/team/week-average")
+def get_weekly_average_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.get_weekly_average()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
-@router.get("/team/{team_name}/week-average")
-def get_weekly_average_route(league: League = Depends(services.get_league), team_name: str = ""):
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    return services.get_weekly_average(team)
+@router.get("/team/best-week")
+def get_best_week_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.get_best_week()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/team/worst-week")
+def get_worst_week_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.get_worst_week()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/team/longest-streak")
+def get_longest_streak_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.get_longest_streak()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))  
+    
 
-@router.get("/team/{team_name}/best-week")
-def get_best_week_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    best_week, best_score = services.get_best_week(team)
-    return {"best_week": best_week, "best_score": best_score}
+@router.get("/team/sleeper")
+def get_sleeper_star_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.get_sleeper_star()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
-@router.get("/team/{team_name}/worst-week")
-def get_worst_week_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    worst_week, worst_score = services.get_worst_week(team)
-    return {"worst_week": worst_week, "worst_score": worst_score}
+@router.get("/team/bust")
+def get_bust_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.get_bust()   
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
-@router.get("/team/{team_name}/longest-streak")
-def get_longest_streak_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    max_win, max_loss = services.get_longest_streak(team)
-    return {"max_win_streak": max_win, "max_loss_streak": max_loss}
+@router.get("/team/clutch")
+def find_clutch_player_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.find_clutch_player()   
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 
-@router.get("/team/{team_name}/sleeper")
-def get_sleeper_star_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    sleeper, avg_points, proj_points = services.get_sleeper_star(team)
-    return {"sleeper": sleeper.name, "avg_points": avg_points, "projected_avg_points": proj_points}
+@router.get("/team/best-matchup")
+def find_best_team_matchup_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.find_best_team_matchup()   
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e)) 
+    
 
-@router.get("/team/{team_name}/bust")
-def get_bust_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    bust, avg_points, proj_points = services.get_bust(team)
-    return {"bust": bust.name, "avg_points": avg_points, "projected_avg_points": proj_points}
-
-@router.get("/team/{team_name}/clutch")
-def find_clutch_player_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    return services.find_clutch_player(team)
-
-@router.get("/team/{team_name}/best-matchup")
-def find_best_team_matchup_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    return services.find_best_team_matchup(team)
-
-@router.get("/team/{team_name}/worst-matchup")
-def find_worst_team_matchup_route(league: League = Depends(services.get_league), team_name: str = ""):  
-    team = services.get_team_by_name(league, team_name)
-    if not team:
-        raise HTTPException(status_code=404, detail="Team not found")
-    return services.find_worst_team_matchup(team)
+@router.get("/team/worst-matchup")
+def find_worst_team_matchup_route(params: ServiceParams):
+    services = Services(
+        league_id = params.league_id,
+        year = params.year,
+        espn_s2 = params.espn_s2,
+        swid = params.swid
+    )
+    try:
+        return services.find_worst_team_matchup()   
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))  
+    
 
--- app/services.py
+++ app/services.py
@@ -1,171 +1,177 @@
-from espn_api.basketball import League
+import time
+from espn_api.basketball import * # espn api says to only import league, but this way python recognizes player and team class
 import requests
 
-def get_league(league_id: int, season_id: int, espn_s2: str, swid: str) -> League:
-    if not hasattr(get_league, "league"):
-        get_league.league = League(
+# stats does not include playoffs
+class Services:
+
+    # league_id is specific to league, year is season year, espn_s2 and swid are cookies
+    # espn_s2 grants permission to access fantasy league data
+    # swid is specific to user and allows us to idenitfy which team is theirs
+    def __init__(self, league_id: int, year: int, espn_s2: str, swid: str):
+        self.league = League(
             league_id=league_id,
-            season_id=season_id,
+            year=year,
             espn_s2=espn_s2,
             swid=swid
         )
-    return get_league.league
-    # what the freak is dependency injection
-
-
-def get_team_by_name(league: League, team_name: str):
-    for team in league.teams:
-        if team_name.lower() in team.team_name.lower():
-            return team
-    return None
-
-def find_trae_young(team):
-
-    #Im gonna find trae young!
-    for player in team.roster:
-        if "trae young" in player.name.lower():
-            return "found trae young!"
+        self.team = None
         
-        
-    return "didn't find trae young aw man"
+        for team in self.league.teams:
+            for user in team.owners:
+                if swid == user.get('id'):
+                    self.team = team
 
-def get_total_points(team):
-    total_points = sum(player.fantasy_points for player in team.roster)
-    return {
-        "total_fantasy_points": total_points
-    }
+        if self.team is None:
+            print("User's team not found in this league.")
 
-def get_weekly_average(team): 
-    total_points = sum(player.fantasy_points for player in team.roster)
-    return total_points / 20 
+    def find_trae_young(self) -> str:
 
-def get_best_week(team):
-    best_week = 0
-    best_score = 0
-    for week in range(1, 21):
-        if team.scores[week - 1] > best_score:
-            best_week = week
-            best_score = team.scores[week - 1]
-    return best_week, best_score
+        #Im gonna find trae young!
+        for player in self.team.roster:
+            if "trae young" in player.name.lower():
+                return "found trae young!"
+              
+        return "didn't find trae young aw man"
 
-def get_worst_week(team):
-    worst_week = 0
-    worst_score = team.scores[0]
-    for week in range(2, 21):
-        if team.scores[week - 1] < worst_score:
-            worst_week = week
-            worst_score = team.scores[week - 1]
-    return worst_week, worst_score
+    # def get_total_points(self): - > team class has points_for
 
-def get_longest_streak(team):
-    win_count = 0
-    max_win = 0
-    loss_count = 0
-    max_loss = 0
-    for i in range(1, 21):
-        if loss_count > max_loss:
-            max_loss = loss_count
-        if win_count > max_win:
-            max_win = win_count
-        if team.outcomes[i] == "WIN":
-            win_count+=1
-            loss_count = 0
-        if team.outcomes[i] == "LOSS":
-            loss_count+=1
-            win_count = 0
-        if team.outcomes[i] == "TIE":
-            win_count = 0
-            loss_count = 0
-    return max_win, max_loss
+    def get_weekly_average(self) -> float: 
+        return self.team.points_for / 20 
 
-def get_sleeper_star(team):
-    sleeper = team.roster[0]
-    final_diff = sleeper.avg_points - sleeper.projected_avg_points + sleeper.total_points - sleeper.projected_total_points
-    for player in team.roster:
-        new_combined_diff = player.avg_points - player.projected_avg_points + player.total_points - player.projected_total_points
-        if new_combined_diff > final_diff:
-            final_diff = new_combined_diff
-            sleeper = player
-    return sleeper, sleeper.avg_points, sleeper.projected_avg_points
+    def get_best_week(self) -> tuple[int, int]:
+        best_week = 0
+        best_score = 0
+        for week in range(20):
+            score = self.team.schedule[week].home_final_score if (self.team == self.team.schedule[week].home_team) else self.team.schedule[week].away_final_score 
+            if score > best_score:
+                best_week = week + 1
+                best_score = score
+        return best_week, best_score
 
-def get_bust(team): 
-    bust = team.roster[0]
-    final_diff = bust.projected_avg_points - bust.avg_points + bust.projected_total_points - bust.total_points
-    for player in team.roster:
-        new_combined_diff = player.projected_avg_points - player.avg_points + player.projected_total_points - player.total_points
-        if new_combined_diff > final_diff:
-            final_diff = new_combined_diff
-            bust = player
-    return bust, bust.avg_points, bust.projected_avg_points
+    def get_worst_week(self) -> tuple[int, int]:
+        worst_week = 0
+        worst_score = float('inf')
+        for week in range(20):
+            score = self.team.schedule[week].home_final_score if (self.team == self.team.schedule[week].home_team) else self.team.schedule[week].away_final_score
+            if score < worst_score:
+                worst_week = week + 1
+                worst_score = score 
+        return worst_week, worst_score
 
-def find_clutch_player(team):  #check method cause might not work well?
-    point_diff_threshold = 100
-    clutch_player = None
-    max_points = -1
+    def get_longest_streak(self) -> tuple[int, int]:
+        win_count = 0
+        max_win = 0
+        loss_count = 0
+        max_loss = 0
+        for matchup in self.team.schedule:
+            if (matchup.home_team == self.team and matchup.winner == 'HOME') or (matchup.home_team != self.team and matchup.winner == 'AWAY'):
+                win_count += 1
+                loss_count = 0
+            else:
+                win_count = 0
+                loss_count += 1
 
-    for matchup in team.schedule:
-        home_team_id = matchup.get("home", {}).get("teamId")
-        away_team_id = matchup.get("away", {}).get("teamId")
+            max_win = max(max_win, win_count)
+            max_loss = max(max_loss, loss_count)
+            
+        return max_win, max_loss
 
-        if team.team_id not in (home_team_id, away_team_id):
-            continue
+    def get_sleeper_star(self) -> tuple[Player, float, float]:
+        sleeper = None
+        final_diff = 0
+        for player in self.team.roster:
+            new_combined_diff = player.avg_points - player.projected_avg_points + player.total_points - player.projected_total_points
+            if new_combined_diff > final_diff:
+                final_diff = new_combined_diff
+                sleeper = player
+        return sleeper, sleeper.avg_points, sleeper.projected_avg_points
 
-        home_score = matchup.get("home", {}).get("totalPoints", 0)
-        away_score = matchup.get("away", {}).get("totalPoints", 0)
-        point_diff = abs(home_score - away_score)
+    def get_bust(self) -> tuple[Player, float, float]: 
+        bust = None
+        final_diff = 0
+        for player in self.team.roster:
+            new_combined_diff = player.projected_avg_points - player.avg_points + player.projected_total_points - player.total_points
+            if new_combined_diff > final_diff:
+                final_diff = new_combined_diff
+                bust = player
+        return bust, bust.avg_points, bust.projected_avg_points
 
-        if point_diff <= point_diff_threshold:
-            final_game = matchup.get("home", {}).get("rosterForCurrentScoringPeriod", {}).get("entries", [])
-            final_game += matchup.get("away", {}).get("rosterForCurrentScoringPeriod", {}).get("entries", [])
+    def find_clutch_player(self) -> Player | str:  #this method should work but doesnt
+        point_diff_threshold = 100
+        count = {}
+        for i in range(20):
+            max_points = -1
+            clutch_player = None
 
-            for entry in final_game:
-                player = entry.get("playerPoolEntry", {}).get("player", {})
-                player_name = player.get("fullName")
-                points = entry.get("appliedStatTotal", 0)
+            # checking if you won, and if point diff was close enough
+            if (self.team.schedule[i].home_team == self.team and self.team.schedule[i].winner == 'HOME') or (self.team.schedule[i].home_team != self.team and self.team.schedule[i].winner == 'AWAY'):
+                home_score = self.team.schedule[i].home_final_score
+                away_score = self.team.schedule[i].away_final_score
+                point_diff = abs(home_score - away_score)
 
-                if points > max_points:
-                    max_points = points
-                    clutch_player = player_name
-    return clutch_player if clutch_player else "No clutch player found. you drafted tatum?"
+                if point_diff <= point_diff_threshold:
+                    final_games = self.league.box_scores(i + 1, 6, False)
+                    for box_score in final_games:
+                        if (s.team == box_score.home_team) or (s.team == box_score.away_team):
+                            matchup = box_score
+                            print(box_score.home_lineup) # home lineup pops up empty??????
+                            break
+                    print(matchup.home_lineup) # 
+                    lineup  = matchup.home_lineup if matchup.home_team == self.team else matchup.away_lineup
+                    print(lineup) #
+                    for player in lineup:
+                        if player.slot_position != 'BE' and player.slot_position != 'IR' and player.points > max_points and player.points > player.avg_points: 
+                            max_points = player.points
+                            clutch_player = player
+                    if clutch_player is not None:
+                        if clutch_player in count:
+                            count[clutch_player] += 1
+                        else:
+                            count[clutch_player] = 1
+    
+        return max(count, key=count.get) if count else "No clutch player found. you drafted tatum?"
 
-def find_best_team_matchup(team):
-    win_counts = {}
+    def find_best_team_matchup(self) -> str | Team:
+        win_counts = {}
 
-    for matchup in team.schedule:
-        home_team_id = matchup.get("home", {}).get("teamId")
-        away_team_id = matchup.get("away", {}).get("teamId")
-        home_score = matchup.get("home", {}).get("totalPoints", 0)
-        away_score = matchup.get("away", {}).get("totalPoints", 0)
+        for matchup in self.team.schedule:
+            home_score = matchup.home_final_score
+            away_score = matchup.away_final_score
 
-        if home_team_id == team.team_id and home_score > away_score:
-            win_counts[away_team_id] = win_counts.get(away_team_id, 0) + 1
-        elif away_team_id == team.team_id and away_score > home_score:
-            win_counts[home_team_id] = win_counts.get(home_team_id, 0) + 1
+            if matchup.home_team == self.team.team_id and home_score > away_score:
+                win_counts[matchup.away_team] = win_counts.get(matchup.away_team, 0) + 1
+            elif matchup.away_team_id == self.team.team_id and away_score > home_score:
+                win_counts[matchup.home_team] = win_counts.get(matchup.home_team, 0) + 1
 
-    if not win_counts:
-        return "How did you not win a single game you bum. quit."
+        if not win_counts:
+            return "How did you not win a single game you bum. quit."
 
-    best_opponent = max(win_counts, key=win_counts.get)
-    return f"You beat {best_opponent} a total of {win_counts[best_opponent]} times."
+        best_opponent = max(win_counts, key=win_counts.get)
+        return best_opponent
 
-def find_worst_team_matchup(team):
-    loss_counts = {}
+    def find_worst_team_matchup(self) -> str | Team:
+        loss_counts = {}
 
-    for matchup in team.schedule:
-        home_team_id = matchup.get("home", {}).get("teamId")
-        away_team_id = matchup.get("away", {}).get("teamId")
-        home_score = matchup.get("home", {}).get("totalPoints", 0)
-        away_score = matchup.get("away", {}).get("totalPoints", 0)
+        for matchup in team.schedule:
+            home_score = matchup.home_final_score
+            away_score = matchup.away_final_score
 
-        if home_team_id == team.team_id and home_score < away_score:
-            loss_counts[away_team_id] = loss_counts.get(away_team_id, 0) + 1
-        elif away_team_id == team.team_id and away_score < home_score:
-            loss_counts[home_team_id] = loss_counts.get(home_team_id, 0) + 1
+            if matchup.home_team == self.team.team_id and home_score < away_score:
+                loss_counts[matchup.away_team] = loss_counts.get(matchup.away_team, 0) + 1
+            elif matchup.away_team_id == self.team.team_id and away_score < home_score:
+                loss_counts[matchup.home_team] = loss_counts.get(matchup.home_team, 0) + 1
 
-    if not loss_counts:
-        return "Maybe your the goat, maybe ur name is jacob. Maybe both"
+        if not loss_counts:
+            return "Maybe your the goat, maybe ur name is jacob. Maybe both"
 
-    worst_opponent = max(loss_counts, key=loss_counts.get)
-    return f"You lost to {worst_opponent} a total of {loss_counts[worst_opponent]} times. go die"
+        worst_opponent = max(loss_counts, key=loss_counts.get)
+        return worst_opponent
+    
 
+# my swid used for testing 
+# s = Services(league_id=332773775, year=2025, espn_s2='AEB%2FraVAzJUuPQQx%2FZbZyHlQBgCLq%2FRJZeRW%2FD2PS9L1c89tj7UCmG7Y8jGvoYKhToVRtrWmOV8wHyGr8PkOlQJ%2Bc6WyPrTJHE8s2fgroHPV2Z3vA3Hp1QbO0ZlHFu0YvNBT1OMvExX1l7vPZPi5Is4Fmqx8AJDu8aGb5sdXtY5G1oEJ5imB9sjcwj3QUnA0lBdCWbQ%2BUcs%2FDnBNkWDd%2Fe191amCJFp7S0%2BnH1ut5HMOPlo%2B6gh3FhScoJQNIhqkGL2gQr0Bv0WIrSA%2F7Cg8ywpJPBwCDr9tpfwAmfqFWYzABQ%3D%3D', swid='{1A576FEF-EB0A-4EAC-A122-54A7CB7DD0FF}')
 
+    
+
+   
